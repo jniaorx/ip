caa35d89e6afc420e4e94c55cc97d30bb7ade2c5
Janelle <janelletanrx@gmail.com>
2025-01-21 17:07:42 +0800

    Added some comments.

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+// to manage tasks that user inputs
 public class TaskManager {
     private ArrayList<String> tasksList = new ArrayList<>();
 
@@ -8,10 +8,13 @@ public class TaskManager {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String userInput = scanner.nextLine();
+            // stop if user inputs bye
             if (userInput.equalsIgnoreCase("Bye")) {
                 break;
+            // print task list if user inputs list
             } else if (userInput.equalsIgnoreCase("List")) {
                 printTaskList();
+            // add task to task list
             } else {
                 addTask(userInput);
             }
@@ -20,6 +23,7 @@ public class TaskManager {
         scanner.close();
     }
 
+    // method to add task to task list
     public void addTask(String task) {
         tasksList.add(task);
         System.out.println("    __________________");
@@ -27,6 +31,7 @@ public class TaskManager {
         System.out.println("    __________________");
     }
 
+    // method to print out task list
     public void printTaskList() {
         if (tasksList.isEmpty()) {
             System.out.println("Your task list is empty now! Please add tasks!");

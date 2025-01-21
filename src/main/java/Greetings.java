2260cb4311da7fc1237418b91e7325d4a40ddec1
Janelle <janelletanrx@gmail.com>
2025-01-21 11:45:31 +0800

    Added hi and bye messages.

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
 public class Greetings {
+    public void greetUser() {
+        Scanner scanner = new Scanner(System.in);
 
+        // greet user
+        System.out.println("__________________");
+        System.out.println("Sup! I am IntroBlaise.");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________");
+
+        // wait for user input
+        String userInput = scanner.nextLine();
+
+        // say bye to user
+        System.out.println("__________________");
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("__________________");
+
+        // close scanner
+        scanner.close();
+    }
 }

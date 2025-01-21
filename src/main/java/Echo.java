c9f4a196edc45fe47ed72b736dacbd9b1f56effd
Janelle <janelletanrx@gmail.com>
2025-01-21 15:51:45 +0800

    Added indentation.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -7,8 +7,9 @@ public class Echo implements ChatAction{
         // while userInput is not "bye", repeat user input
         while (!userInput.equals("bye")) {
             // repeat user input
-            System.out.println(userInput);
-            System.out.println("__________________");
+            System.out.println("    __________________");
+            System.out.println("    " + userInput);
+            System.out.println("    __________________");
 
             // wait for user input
             userInput = scanner.nextLine();
--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -4,16 +4,16 @@ public class Greetings implements ChatAction {
     @Override
     public void execute(Scanner scanner) {
         // greet user
-        System.out.println("__________________");
-        System.out.println("Sup! I am IntroBlaise.");
-        System.out.println("What can I do for you?");
-        System.out.println("__________________");
+        System.out.println("    __________________");
+        System.out.println("    Sup! I am IntroBlaise.");
+        System.out.println("    What can I do for you?");
+        System.out.println("    __________________");
     }
 
     public void sayBye() {
         // if user input is "bye", say bye to user
-        System.out.println("__________________");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("__________________");
+        System.out.println("    __________________");
+        System.out.println("    Bye! Hope to see you again soon!");
+        System.out.println("    __________________");
     }
 }

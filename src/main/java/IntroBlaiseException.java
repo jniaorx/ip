520ba1011231e0357d05f413b9e5b8fda7e91bdf
Janelle <janelletanrx@gmail.com>
2025-01-24 01:01:49 +0800

    Modified comments in IntroBlaiseException class.

--- src/main/java/IntroBlaiseException.java
+++ src/main/java/IntroBlaiseException.java
@@ -1,4 +1,14 @@
+/**
+ * A custom exception class for the IntroBlaise bot.
+ * The {@code IntroBlaiseException} class is used to represent application-specific errors
+ * that may occur during the execution of the IntroBlaise program.
+ */
 public class IntroBlaiseException extends Exception {
+
+    /**
+     * Constructs a new IntroBlaiseException with the specified detail message.
+     * @param exception The detail message that explains the reason for this exception.
+     */
     public IntroBlaiseException(String exception) {
         super(exception);
     }

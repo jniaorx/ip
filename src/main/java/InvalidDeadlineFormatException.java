0c443dc3a8f346de179646e1ff885899e6c3f2c4
Janelle <janelletanrx@gmail.com>
2025-01-24 01:03:13 +0800

    Modified comments in InvalidDeadlineFormatException class.

--- src/main/java/InvalidDeadlineFormatException.java
+++ src/main/java/InvalidDeadlineFormatException.java
@@ -1,4 +1,12 @@
+/**
+ * Exception class representing an error condition where user inputs
+ * incorrect format for a Deadline task.
+ */
 public class InvalidDeadlineFormatException extends IntroBlaiseException{
+    /**
+     * Constructs a new {@code InvalidDeadlineException} with the specified detail message.
+     * @param exception The detail message that explains the reason for this exception.
+     */
     public InvalidDeadlineFormatException(String exception) {
         super(exception);
     }

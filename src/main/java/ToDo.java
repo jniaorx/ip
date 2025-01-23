5361299cbeabffba34e15aee6840dc4e2f328b4f
Janelle <janelletanrx@gmail.com>
2025-01-24 01:26:36 +0800

    Modified comments in ToDo class.

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,9 +1,21 @@
-// tasks without any date/time attached to it
+/**
+ * Represents a task without any date/time attached to it.
+ * This class extends the {@code Task} class and add the task without any date/time attached
+ * to the list. It overrides the {@code toString()} method to provide a customized string representation.
+ */
 public class ToDo extends Task{
+    /**
+     * Construcs a new {@code ToDo} task with the specified description.
+     * @param description The description of the task.
+     */
     public ToDo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of the todo task, including its type and description.
+     * @return A string in the format "[T][status] description"
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();

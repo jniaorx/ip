184af7ce83b565d343a2aa1a2d8d93173ca30b43
Janelle <janelletanrx@gmail.com>
2025-01-23 17:26:56 +0800

    Modified greetings.

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -8,10 +8,10 @@ public class Greetings {
         // greet user
         System.out.println("    __________________");
         System.out.println("    Sup! I am IntroBlaise. What's your name?");
-        if (scanner.hasNextLine()) {      // Add check for available input
+        if (scanner.hasNextLine()) {      // check for avaiable input
             String name = scanner.nextLine();
             System.out.println("    __________________");
-            System.out.println("    Hi " + name + "! What can I do for you?");
+            System.out.println("    Hi " + name + "!(ʘ‿ʘ)╯ Nice to meet you! You can start adding tasks to your very own task list! :)");
             System.out.println("    __________________");
         }
     }
@@ -19,7 +19,7 @@ public class Greetings {
     public void sayBye() {
         // if user input is "bye", say bye to user
         System.out.println("    __________________");
-        System.out.println("    Bye! Hope to see you again soon!");
+        System.out.println("    Bye! Hope to see you again soon! (╥﹏╥)");
         System.out.println("    __________________");
     }
 }
